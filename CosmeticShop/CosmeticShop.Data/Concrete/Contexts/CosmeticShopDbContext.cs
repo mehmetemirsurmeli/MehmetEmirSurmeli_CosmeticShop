32ea9fb28122ddcb8ea5a3a10253f2ecea5107cf
mehmetemirsurmeli <mehmet.surmeli1903@gmail.com>
2024-03-16 20:13:43 +0300

    CosmeticShop.Data katmanı düzenleniyor
    data katmanı yapılma aşamasında.

---
+++ CosmeticShop/CosmeticShop.Data/Concrete/Contexts/CosmeticShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CosmeticShop.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmeticShop.Data.Concrete.Contexts
+{
+    public class CosmeticShopDbContext
+    {
+        public CosmeticShopDbContext(DbContextOptions options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ CosmeticShop/CosmeticShop.Entity/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using CosmeticShop.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CosmeticShop.Entity.Concrete
+{
+    public class Category:GeneralEntity,IMainEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+        public string CategoryDescription { get; set; }
+
+    }
+}
