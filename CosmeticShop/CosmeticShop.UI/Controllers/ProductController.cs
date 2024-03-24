a2824c821bcc7b29ff8dbe0b9eabd6628e4ed2f3
mehmetemirsurmeli <mehmet.surmeli1903@gmail.com>
2024-03-24 15:59:44 +0300

    product controller oluşturuldu ürünler listelendi. refactoringler yapılıcak

---
+++ CosmeticShop/CosmeticShop.UI/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using CosmeticShop.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmeticShop.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productManager;
+        private readonly ICategoryService _categoryManager;
+
+        public ProductController(IProductService productManager, ICategoryService categoryManager)
+        {
+            _productManager = productManager;
+            _categoryManager = categoryManager;
+        }
+
+        public async Task<IActionResult> Index(string categoryUrl = null)
+        {
+            var products = String.IsNullOrEmpty(categoryUrl) ? 
+                await _productManager.GetAllNonDeletedAsync() :
+                await _productManager.GetProductsByCategoryUrlAsync(categoryUrl);
+
+            var category = !String.IsNullOrEmpty(categoryUrl) ? await _categoryManager.GetByUrlAsync(categoryUrl) : null;
+
+            ViewBag.CategoryName = category != null ? category.Data.Name : null;
+
+            return View(products.Data);
+        }
+    }
+}
