d42b20604825fed37720ee93ef4a97a2d5768dd8
mehmetemirsurmeli <mehmet.surmeli1903@gmail.com>
2024-03-25 16:48:13 +0300

    hata çözüldü loyoutta dosya yolunun başına ~ vermem gerekiyormuş.

---
+++ CosmeticShop/CosmeticShop.UI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using CosmeticShop.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmeticShop.UI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productManager;
+        private readonly ICategoryService _categoryManager;
+
+        public ProductsController(IProductService productManager, ICategoryService categoryManager)
+        {
+            _productManager = productManager;
+            _categoryManager = categoryManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productManager.GetAllNonDeletedAsync(false);
+            return View(products.Data);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View();
+        }
+    }
+}
