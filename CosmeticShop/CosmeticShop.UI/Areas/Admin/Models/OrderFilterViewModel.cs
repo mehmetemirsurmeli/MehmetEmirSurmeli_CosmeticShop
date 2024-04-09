b34b3dee051754fd5ad7f9b7891956a3d380511b
mehmetemirsurmeli <mehmet.surmeli1903@gmail.com>
2024-04-09 17:13:02 +0300

    Revert "Admin area eklendi düzenlemeler yapılıcak resim sürekle bırak şeklinde güncellenicek"
    This reverts commit 214b5df1d28e1e0c7447eb7265b1fea22289eebf.

---
+++ CosmeticShop/CosmeticShop.UI/Areas/Admin/Models/OrderFilterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using CosmeticShop.Shared.ViewModels;
+
+namespace CosmeticShop.UI.Areas.Admin.Models
+{
+    public class OrderFilterViewModel
+    {
+        public List<AdminOrderViewModel> Orders { get; set; }
+        public List<SelectListItem> ProductListItems { get; set; }
+    }
+}
